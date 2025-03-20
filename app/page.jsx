803d78a9d3cf2ec8a8edeae1727f648833466650
Aman Tripathi <amanttripathi02@gmail.com>
2025-03-20 05:32:25 +0530

    created socials component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import {FiDownload} from "react-icons/fi"
 import Socials from "@/components/Socials"
-
+import Photo from "@/components/Photo"  
 const Home = () => {
   return (
   <section className="h-full">
@@ -26,12 +26,16 @@ const Home = () => {
               <FiDownload  className="text-xl"/>
             </Button>
             <div className="mb-8 xl:mb-0">
-              <Socials/>
+              <Socials containerStyles= "flex gap-6" iconStyles="w-9 h-9 border
+              border-accent rounded-full flex justify-center items-center 
+              text-accent text-base hover:bg-accent hover:text-primary hover: transition-all duration-500"/>
             </div>
           </div>
         </div>
         {}
-        <div>photo</div>
+        <div>
+          <Photo/>
+        </div>
       </div>
     </div>
   </section>
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,10 +1,25 @@
+import Link from 'next/link';
+import {FaGithub, FaLinkedin, FaTwitter,} from 'react-icons/fa';
+import {TbBrandLeetcode} from 'react-icons/tb';
 
-const Socials = () => {
+const socials = [
+  {icon: <FaGithub/>, path: ''},
+  {icon: <FaLinkedin/>, path: ''},
+  {icon: <FaTwitter/>, path: ''},
+  {icon: <TbBrandLeetcode/>, path: ''},
+];
+const Socials = ({containerStyles, iconStyles}) => {
   return (
-    <div>
-      Socials
+    <div className={containerStyles}>
+      {socials.map((item,index)=>{
+        return(
+          <Link key = {index} href={item.path} className={iconStyles}>
+              {item.icon}
+          </Link>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Socials
